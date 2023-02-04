3c5f8fa5f3eaf622696993ba0b3993afd6a43a20
Atsukoro1 <dornicakkuba@gmail.com>
2023-02-04 17:26:52 +0100

    feat: 🎸 .env changes and MinIO setup

--- store/medusa-config.js
+++ store/medusa-config.js
@@ -1,25 +1,9 @@
 const dotenv = require('dotenv')
 
-let ENV_FILE_NAME = '';
-switch (process.env.NODE_ENV) {
-	case 'production':
-		ENV_FILE_NAME = '.env.production';
-		break;
-	case 'staging':
-		ENV_FILE_NAME = '.env.staging';
-		break;
-	case 'test':
-		ENV_FILE_NAME = '.env.test';
-		break;
-	case 'development':
-	default:
-		ENV_FILE_NAME = '.env';
-		break;
-}
-
 try {
-	dotenv.config({ path: process.cwd() + '/' + ENV_FILE_NAME });
+	dotenv.config({ path: process.cwd() + '/' + '.env' });
 } catch (e) {
+	console.error(`Failed to load environment variables: ${e}`);
 }
 
 // CORS when consuming Medusa from admin
@@ -35,14 +19,22 @@ const DATABASE_URL =
 // Medusa uses Redis, so this needs configuration as well
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
-// Stripe keys
-const STRIPE_API_KEY = process.env.STRIPE_API_KEY || "";
-const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET || "";
-
 // This is the place to include plugins. See API documentation for a thorough guide on plugins.
 const plugins = [
-  `medusa-fulfillment-manual`,
-  `medusa-payment-manual`,
+  {
+	resolve: 'medusa-fulfillment-manual'
+  },
+  {
+	resolve: 'medusa-payment-manual'
+  },
+  {
+	resolve: 'medusa-file-minio',
+	options: {
+		private_bucket: process.env.MINIO_PRIVATE_BUCKET,
+		private_access_key_id: process.env.MINIO_PRIVATE_ACCESS_KEY,
+        private_secret_access_key: process.env.MINIO_PRIVATE_SECRET_KEY
+	}
+  }
 ];
 
 module.exports = {
