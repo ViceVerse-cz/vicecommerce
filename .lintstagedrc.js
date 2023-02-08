61402abb3c4ac4c733726b35b1647c04f570c618
lnxcz <frnoch@pm.me>
2023-02-08 23:42:16 +0100

    fix(panel): :bug: lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "*.{js,jsx,ts,tsx}": ["rome format --write", "rome check --apply"],
+  "*.{js,jsx,ts,tsx}": ["rome format --write", "rome check --apply-suggested"],
 };
--- apps/panel/src/main.tsx
+++ apps/panel/src/main.tsx
@@ -40,7 +40,7 @@ const root = createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <Page>
-      <App></App>
+      <App />
     </Page>
   </React.StrictMode>,
 );
