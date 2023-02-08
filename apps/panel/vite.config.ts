6c71569348dab6d4c391f3cf51dc43193c59995c
lnxcz <frnoch@pm.me>
2023-02-08 23:38:00 +0100

    chore(panel): :hammer: remove backwards comp with gatsby

--- apps/panel/vite.config.ts
+++ apps/panel/vite.config.ts
@@ -1,36 +1,29 @@
-import path from 'path';
-import { env } from 'process';
-import dns from 'dns';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from "path";
+import { env } from "process";
+import dns from "dns";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // Resolve localhost for Node v16 and older.
 // @see https://vitejs.dev/config/server-options.html#server-host.
-dns.setDefaultResultOrder('verbatim');
+dns.setDefaultResultOrder("verbatim");
 
 export default defineConfig({
   plugins: [react()],
   // Backwards-compat with Gatsby.
-  publicDir: 'static',
+  publicDir: "static",
   build: {
-    outDir: 'public',
+    outDir: "public",
   },
   resolve: {
     alias: {
-      gatsby: path.resolve(__dirname, 'src/compat/gatsby-compat.tsx'),
-      '@reach/router': path.resolve(__dirname, 'src/compat/reach-router-compat.tsx'),
+      "@reach/router": path.resolve(__dirname, "src/compat/reach-router-compat.tsx"),
     },
   },
   define: {
-    __MEDUSA_BACKEND_URL__: JSON.stringify(
-      env.MEDUSA_BACKEND_URL ||
-        // Backwards-compat with Gatsby.
-        env.GATSBY_MEDUSA_BACKEND_URL ||
-        env.GATSBY_STORE_URL ||
-        '',
-    ),
+    __MEDUSA_BACKEND_URL__: JSON.stringify(env.MEDUSA_BACKEND_URL || ""),
   },
   optimizeDeps: {
-    exclude: ['typeorm', 'medusa-interfaces'],
+    exclude: ["typeorm", "medusa-interfaces"],
   },
 });
