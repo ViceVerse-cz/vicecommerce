0902083c3795babaa9c0b3db5ec5084a4eca64ff
Atsukoro1 <dornicakkuba@gmail.com>
2023-02-08 22:16:17 +0100

    feat: 🎸 Changes

--- apps/store/src/main.ts
+++ apps/store/src/main.ts
@@ -1,18 +1,18 @@
-import { ShippingModule } from './modules/shipping/shipping.module';
-import { Medusa } from 'medusa-extender';
-import config from '../medusa-config';
-import express = require('express');
-import { resolve } from 'path';
+import { ShippingModule } from "./modules/shipping/shipping.module";
+import { Medusa } from "medusa-extender";
+import config from "../medusa-config";
+import express = require("express");
+import { resolve } from "path";
 
 async function bootstrap() {
   const expressInstance = express();
 
-  await new Medusa(resolve(__dirname, '..'), expressInstance).load([ShippingModule]);
+  await new Medusa(resolve(__dirname, ".."), expressInstance).load([ShippingModule]);
 
   const port = config.projectConfig.port || 9000;
 
   expressInstance.listen(port, () => {
-    console.info('Server successfully started on port ' + port);
+    console.info(`Server successfully started on port ${port}`);
   });
 }
 
