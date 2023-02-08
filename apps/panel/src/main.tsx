098f73450c87cb30ba85345b3207e652bbf61037
lnxcz <frnoch@pm.me>
2023-02-08 23:39:53 +0100

    fix(panel): :bug: test

--- apps/panel/src/main.tsx
+++ apps/panel/src/main.tsx
@@ -36,11 +36,11 @@ const Page = ({ children }: PropsWithChildren) => {
   );
 };
 
-const root = createRoot(documnt.getElementById("root")!);
+const root = createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <Page>
-      <App />
+      <App></App>
     </Page>
   </React.StrictMode>,
 );
