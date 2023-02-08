9e2cf525c159aa72312629e91ac2b39d57fd3477
Atsukoro1 <dornicakkuba@gmail.com>
2023-02-08 22:10:36 +0100

    feat: 🎸 Parse

--- apps/store/src/modules/shipping/zasilkovna.service.ts
+++ apps/store/src/modules/shipping/zasilkovna.service.ts
@@ -1,10 +1,11 @@
-import { FulfillmentProvider } from 'medusa-extender';
-import { FulfillmentService } from 'medusa-interfaces';
-import { Order, Item, Fulfillment, Data } from '@medusajs/medusa';
+import { FulfillmentProvider } from "medusa-extender";
+import { FulfillmentService } from "medusa-interfaces";
+import { Order, Item, Fulfillment, Data } from "@medusajs/medusa";
+import js2xmlparser from "js2xmlparser";
 
 @FulfillmentProvider()
 export default class extends FulfillmentService {
-  static identifier = 'zasilkovna';
+  static identifier = "zasilkovna";
 
   constructor() {
     super();
@@ -13,13 +14,13 @@ export default class extends FulfillmentService {
   getFulfillmentOptions() {
     return [
       {
-        id: 'zasilkovna-dobirka',
-        name: 'Zásilkovna - Dobírka',
+        id: "zasilkovna-dobirka",
+        name: "Zásilkovna - Dobírka",
         is_return: false,
       },
       {
-        id: 'zasilkovna-box',
-        name: 'Zásilkovna - Box',
+        id: "zasilkovna-box",
+        name: "Zásilkovna - Box",
         is_return: false,
       },
     ];
@@ -29,12 +30,22 @@ export default class extends FulfillmentService {
     return true;
   }
 
-  async createFulfillment(
-    data: Data,
-    items: Item[],
-    order: Order,
-    fulfillment: Fulfillment,
-  ): Promise<void> {
+  async createFulfillment(data: Data, items: Item[], order: Order, fulfillment: Fulfillment): Promise<void> {
+    const payload = {
+      apiPassword: "goofyahhklic",
+      packetAttributes: {
+        number: order.id,
+        name: order.customer.first_name,
+        surname: order.customer.last_name,
+        email: order.customer.email,
+        addressId: 55,
+        value: 43,
+        eshop: "vasky.cz",
+      },
+    };
+
+    console.log(js2xmlparser.parse("createPacket", payload));
+
     return Promise.resolve();
   }
 
