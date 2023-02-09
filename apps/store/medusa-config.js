fb751279499d2f8e99a33989e933d9f985b1be5a
Atsukoro1 <dornicakkuba@gmail.com>
2023-02-09 14:10:19 +0100

    feat: 🎸 Using root folder environment variables in store feed

--- apps/store/medusa-config.js
+++ apps/store/medusa-config.js
@@ -12,6 +12,71 @@ const DATABASE_URL = process.env.DATABASE_URL;
 // Medusa uses Redis, so this needs configuration as well
 const REDIS_URL = "redis://localhost:6379";
 
+/**
+ * {
+      "name": "Zasilkovna - Box",
+      "region_id": "test-region-eu",
+      "provider_id": "zasilkovna-fulfillment",
+      "data": {
+        "id": "zasilkovna-box"
+      },
+      "price_type": "flat_rate",
+      "is_return": false,
+      "amount": 1000
+    },
+    {
+      "name": "Zasilkovna - Delivery",
+      "region_id": "test-region-eu",
+      "provider_id": "zasilkovna-fulfillment",
+      "data": {
+        "id": "zasilkovna-delivery"
+      },
+      "price_type": "flat_rate",
+      "is_return": false,
+      "amount": 1000
+    }
+ */
+
+// import { Cart, Data } from "@medusajs/medusa";
+// import { FulfillmentService } from "medusa-interfaces"
+
+// export default class ZasilkovnaFulfillment extends FulfillmentService {
+//     static identifier = 'zasilkovna-fulfillment';
+
+//     constructor() {
+//         super();
+//     }
+
+//     override async getFulfillmentOptions() {
+//         return [
+//           {
+//             id: "zasilkovna-box",
+//             name: "Zasilkovna - Box",
+//             is_return: false
+//           },
+//           {
+//             id: "zasilkovna-delivery",
+//             name: "Zasilkovna - Dobirka",
+//             is_return: false
+//           },
+//         ]
+//     }
+
+//     override async validateOption(data: Data): Promise<boolean> {
+//         return true;
+//     }
+
+//     override async validateFulfillmentData(
+//         optionData,
+//         data,
+//         cart: Cart
+//     ) {
+//         return {
+//           ...data,
+//         }
+//     }
+// }
+
 // This is the place to include plugins. See API documentation for a thorough guide on plugins.
 const plugins = [
   {
@@ -33,16 +98,16 @@ const plugins = [
   },
 
   // Docs: https://docs.medusajs.com/add-plugins/s3
-  // {
-  //   resolve: "medusa-file-s3",
-  //   options: {
-  //     s3_url: process.env.S3_URL,
-  //     bucket: process.env.S3_BUCKET,
-  //     region: process.env.S3_REGION,
-  //     access_key_id: process.env.S3_ACCESS_KEY_ID,
-  //     secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-  //   },
-  // },
+  {
+    resolve: "medusa-file-s3",
+    options: {
+      s3_url: process.env.S3_URL,
+      bucket: process.env.S3_BUCKET,
+      region: process.env.S3_REGION,
+      access_key_id: process.env.S3_ACCESS_KEY_ID,
+      secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  },
 
   // Docs: https://github.com/medusajs/medusa/tree/master/packages/medusa-plugin-meilisearch
   {
