c2c5a90a1c2bec85e3852d02a443ee644e29e209
Atsukoro1 <dornicakkuba@gmail.com>
2023-02-09 08:37:27 +0100

    feat: 🎸 <goofy afh

--- apps/store/medusa-config.js
+++ apps/store/medusa-config.js
@@ -33,16 +33,16 @@ const plugins = [
   },
 
   // Docs: https://docs.medusajs.com/add-plugins/s3
-  // {
-  //   resolve: "medusa-file-s3",
-  //   options: {
-  //     s3_url: process.env.S3_URL,
-  //     bucket: process.env.S3_BUCKET,
-  //     region: process.env.S3_REGION,
-  //     access_key_id: process.env.S3_ACCESS_KEY_ID,
-  //     secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-  //   },
-  // },
+  {
+    resolve: "medusa-file-s3",
+    options: {
+      s3_url: process.env.S3_URL,
+      bucket: process.env.S3_BUCKET,
+      region: process.env.S3_REGION,
+      access_key_id: process.env.S3_ACCESS_KEY_ID,
+      secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+    },
+  },
 
   // Docs: https://github.com/medusajs/medusa/tree/master/packages/medusa-plugin-meilisearch
   {
