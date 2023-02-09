1f3173547fcf11cb11ab8de7b38adc99cdeeef61
lnxcz <frnoch@pm.me>
2023-02-09 18:13:03 +0100

    chore(panel): :hammer: remove public dir

--- apps/panel/public/assets/error-messages-d2d0bb30.js
+++
@@ -1,7 +0,0 @@
-const t = (s) => {
-  var a;
-  var g;
-  let e = (g = (a = s == null ? void 0 : s.response) == null ? void 0 : a.data) == null ? void 0 : g.message;
-  return e[0].message && (e = e[0].message), e || (e = "Something went wrong, Please try again."), e;
-};
-export { t as g };
