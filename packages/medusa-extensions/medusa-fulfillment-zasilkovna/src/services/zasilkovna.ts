b719ebfb3c002b89a7d7f4f6d2074cf1f6b57e58
Atsukoro1 <dornicakkuba@gmail.com>
2023-02-10 11:45:58 +0100

    feat: 🎸 Fulfillment

--- packages/medusa-extensions/medusa-fulfillment-zasilkovna/src/services/zasilkovna.ts
+++ packages/medusa-extensions/medusa-fulfillment-zasilkovna/src/services/zasilkovna.ts
@@ -1,10 +1,14 @@
+import { Data, Fulfillment, LineItem, Order } from "@medusajs/medusa";
 import { FulfillmentService } from "medusa-interfaces";
+import { parse } from "js2xmlparser";
 
 class ZasilkovnaService extends FulfillmentService {
   static identifier = "zasilkovna";
+  apikey: string;
 
-  constructor() {
+  constructor({}, options: { apiKey: string }) {
     super();
+    this.apikey = options.apiKey;
   }
 
   async getFulfillmentOptions() {
@@ -23,37 +27,73 @@ class ZasilkovnaService extends FulfillmentService {
   }
 
   validateFulfillmentData(_, data, cart) {
+    console.log("validate_fulfillment");
     return data;
   }
 
   validateOption(data) {
+    console.log("validate_option");
     return true;
   }
 
   canCalculate() {
+    console.log("can_calculate");
     return false;
   }
 
   calculatePrice() {
+    console.log("calculate_price");
     throw Error("Manual Fulfillment service cannot calculatePrice");
   }
 
   createOrder() {
     // No data is being sent anywhere
+
+    console.log("create order");
     return Promise.resolve({});
   }
 
   createReturn() {
     // No data is being sent anywhere
+
+    console.log("create return");
     return Promise.resolve({});
   }
 
-  createFulfillment() {
-    // No data is being sent anywhere
-    return Promise.resolve({});
+  async createFulfillment(data: Data, items: LineItem[], order: Order, fulfillment: Fulfillment): Promise<void> {
+    const reqData = {
+      apiPassword: this.apikey,
+      packetAttributes: {
+        number: 1,
+        name: order.shipping_address.first_name,
+        surname: order.shipping_address.last_name,
+        email: order.email,
+        phone: order.shipping_address.phone,
+        addressId: 79,
+        value: order.paid_total,
+        eshop: "vasky.cz",
+      },
+    };
+
+    const { status } = await fetch("https://www.zasilkovna.cz/api/rest", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/xml",
+      },
+      body: JSON.stringify(parse("createPacket", reqData)),
+    });
+
+    if (status === 200) {
+      return Promise.resolve();
+    } else {
+      return Promise.reject({
+        error: "Failed to create packet via Zasilkovna provider",
+      });
+    }
   }
 
   cancelFulfillment() {
+    console.log("cancel fulfillment");
     return Promise.resolve({});
   }
 }
